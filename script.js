3b74c98f77e2887a688ddf8bef5745a513353416
@K1t823 <v.drop.direction@gmail.com>
2022-03-26 15:06:11 +0300

    console rezult

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-document.body.innerHTML = "<h1>Hello from JavaScript</h1>"
+"use strict"
+/*const userProfile = {
+   myName: 'Raman',
+   myEmail: "v.drop.direction@gmail.com",
+   myAge: 27
+
+}
+
+
+console.log(userProfile);*/
+
+
+
+let resultOne = "25" - 5;
+console.log(resultOne);
+console.log(typeof resultOne);
+
+
+let resultTwo = 10 * "80";
+console.log(resultTwo);
+console.log(typeof resultTwo);
+
+
+let resultThree = "3" * "js";
+console.log(resultThree);
+console.log(typeof resultThree);
+
